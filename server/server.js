533f85ad7e60c029f0d0f05406fdb14f9d87f5d4
Tasin Ishmam <tasinishmam@gmail.com>
2019-05-21 10:01:45 +0600

    Added chat form and sending messages with jquery

--- server/server.js
+++ server/server.js
@@ -19,14 +19,10 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    callback('This is from the server.');
   });
 
   socket.on('disconnect', () => {
